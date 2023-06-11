f4c3f13c6b36cd344923057887034abb812441ad
Alexey <alexei.trotsik@yandex.ru>
2023-06-11 17:36:31 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    System.Console.Write ($"max = {numberA} ");
+}
+else
+{
+    System.Console.Write ($"max = {numberB} ");
+}
